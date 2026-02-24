480fd1e6d27feb44f24b839077f388c24fd659d7
xmodify <xmodifya@gmail.com>
2026-02-24 18:26:45 +0700

    update

--- database/migrations/2026_02_24_155007_create_productivity_hd_table.php
+++ database/migrations/2026_02_24_155007_create_productivity_hd_table.php
@@ -11,23 +11,25 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('productivity_hd', function (Blueprint $table) {
-            $table->id();
-            $table->date('report_date');
-            $table->string('shift_time', 20);
-            $table->integer('nurse_fulltime')->nullable();
-            $table->integer('nurse_partime')->nullable();
-            $table->integer('nurse_oncall')->nullable();
-            $table->string('recorder')->nullable();
-            $table->string('note')->nullable();
-            $table->integer('patient_all')->nullable();
-            $table->double('nursing_hours', 5, 2)->nullable();
-            $table->double('working_hours', 5, 2)->nullable();
-            $table->double('nhppd', 5, 2)->nullable();
-            $table->double('nurse_shift_time', 5, 2)->nullable();
-            $table->double('productivity', 5, 2)->nullable();
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('productivity_hd')) {
+            Schema::create('productivity_hd', function (Blueprint $table) {
+                $table->id();
+                $table->date('report_date');
+                $table->string('shift_time', 20);
+                $table->integer('nurse_fulltime')->nullable();
+                $table->integer('nurse_partime')->nullable();
+                $table->integer('nurse_oncall')->nullable();
+                $table->string('recorder')->nullable();
+                $table->string('note')->nullable();
+                $table->integer('patient_all')->nullable();
+                $table->double('nursing_hours', 5, 2)->nullable();
+                $table->double('working_hours', 5, 2)->nullable();
+                $table->double('nhppd', 5, 2)->nullable();
+                $table->double('nurse_shift_time', 5, 2)->nullable();
+                $table->double('productivity', 5, 2)->nullable();
+                $table->timestamps();
+            });
+        }
     }
 
     /**
