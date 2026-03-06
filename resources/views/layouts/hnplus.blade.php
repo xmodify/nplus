dc3c20223e0bb25f15bb558bd672a692732aff73
xmodify <xmodifya@gmail.com>
2026-03-06 18:44:50 +0700

    update

--- resources/views/layouts/hnplus.blade.php
+++ resources/views/layouts/hnplus.blade.php
@@ -219,7 +219,7 @@
                     <ul class="navbar-nav ms-auto align-items-center">
                         <li>
                             <div class="badge bg-white text-primary rounded-pill px-3 py-2 shadow-sm fw-bold">
-                                <i class="bi bi-code-slash me-1"></i>V. 69-03-06 18:00
+                                <i class="bi bi-code-slash me-1"></i>V. 69-03-06 19:00
                             </div>
                         </li>
 
